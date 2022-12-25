8ed48c0c2aa6e8b8f07fb0ecc1becf4fe5b61971
Ionut Cicio <ionutcicio.dev@gmail.com>
2022-12-25 16:56:39 +0100

    mux

--- system-verilog/mux.sv
+++ system-verilog/mux.sv
@@ -1,14 +1,30 @@
-module mux  (
-	input logic select,
-	output logic [1:0] out
+module mux2 (
+	input logic [3:0] d0, d1,
+	input logic s,
+	output logic [3:0] y
 );
-
-always_comb 
-	case (select)
-		1'b0: out = 2'b10;
-		1'b1: out = 2'b11;
-		// default: out = 1'b1;
-	endcase
-	
-	
+	assign y = s ? d1 : d0;
 endmodule
+
+module mux4 (
+	input logic [3:0] d0, d1, d10, d11,
+	input logic [2:0] s,
+	output logic [3:0] y
+);
+	assign y = s[1] ? (s[0] ? d11 : d10) : (s[0] ? d1 : d0);
+endmodule
+
+// module mux  (
+// 	input logic select,
+// 	output logic [1:0] out
+// );
+
+// always_comb 
+// 	case (select)
+// 		1'b0: out = 2'b10;
+// 		1'b1: out = 2'b11;
+// 		// default: out = 1'b1;
+// 	endcase
+	
+	
+// endmodule
