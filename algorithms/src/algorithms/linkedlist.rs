2cda8e0b77e69aa453366c33eeb13f28610fa041
Ionut Cicio <ionutcicio.dev@gmail.com>
2023-05-09 17:52:56 +0200

    sync

--- algorithms/src/algorithms/linkedlist.rs
+++ algorithms/src/algorithms/linkedlist.rs
@@ -3,6 +3,11 @@ pub struct LinkedList<T> {
     next: Option<Box<LinkedList<T>>>,
 }
 
+// impl<T> From<Vec<T>> for LinkedList<T> {
+//     fn from(value: Vec<T>) -> Self {
+//     }
+// }
+
 impl<T> LinkedList<T> {
     pub fn new(value: T) -> Self {
         Self { value, next: None }
