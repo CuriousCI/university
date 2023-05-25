5cded5c9563da6b1332132589aabfdf1c4a9cfcc
Ionut Cicio <ionutcicio.dev@gmail.com>
2023-05-25 18:47:25 +0200

    assembly

--- book/assembly/statements/dowhile.asm
+++ book/assembly/statements/dowhile.asm
@@ -1,5 +1,3 @@
-.data
-    
 .text
     li $t0, 0 #; x = 0
     li $t1, 0 #; i = 0
@@ -8,4 +6,3 @@
         addi $t0, $t0, 4 #; x += 4
         addi $t1, $t1, 1 #; i += 1
     blt $t1, 10, do #; if i < 10, repeat the cicle 
-        
--- book/assembly/statements/for.asm
+++ book/assembly/statements/for.asm
@@ -1,11 +1,10 @@
-.data
-
 .text
     li $t0, 0 #; x = 0
     li $t1, 0 #; i = 0
 
     for:
+        beq $t1, 10, end #; if i == 10, end loop 
         add $t0, $t0, $t1 #; x += i
         addi $t1, $t1, 1 #; i += 1 
-        blt $t1, 10, for #; if i <= 10, repeat 
-
+        j for
+    end:
